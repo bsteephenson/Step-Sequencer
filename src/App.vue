e24bbb023e3b2fe89249c9ec5f8c7a5d39a9dbcf
Benjamin Steephenson <benjaminsteephenson@gmail.com>
2016-12-29 14:49:30 -0500

    initial ui

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png"> -->
+    <!-- <hello></hello> -->
+    <line-c :beats=16 :current-note=8 :on-notes= "[3, 7]" ></line-c>
+    <line-c :beats=16 :current-note=2 :on-notes= "[1, 4]" ></line-c>
+    <line-c :beats=16 :current-note=4 :on-notes= "[2, 8]" ></line-c>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+import LineC from './components/Line'
+
+// import Wad from "web-audio-daw"
+
+// var saw = new Wad({source : 'square'})
+// saw.setVolume(.25)
+// saw.play({pitch: "C5"})
+// saw.play({pitch: "E5"})
+// saw.play({pitch: "G5"})
+
+export default {
+  name: 'app',
+  components: {
+    Hello, LineC
+  },
+  data: function() {
+    return {
+      isPlaying: false
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Line.vue
@@ -0,0 +1,44 @@
+<template lang="pug">
+    .box-container
+        .box(v-for="(elem, index) in beatsArray", :class="{active: elem, played: index == currentNote}")
+</template>
+
+<script lang="coffee">
+
+Wad = require('web-audio-daw')
+# saw = new Wad({source : 'square'})
+# saw.setVolume(.25)
+# saw.play({pitch: "C5"})
+# saw.play({pitch: "E5"})
+# saw.play({pitch: "G5"})
+
+module.exports = {
+    name: 'line'
+    props: ['beats', 'currentNote', 'onNotes']
+    computed: {
+        beatsArray: () ->
+            it = new Array(this.beats)
+            for onNote in @onNotes
+                it[onNote] = true
+            console.log it
+            return it
+    }
+}
+
+</script>
+
+<style lang="stylus">
+.box-container
+    display:flex
+    flex-direction: row
+    justify-content: space-between
+.box
+    background-color: alpha(black, .05)
+    margin: 10px
+    height: 50px
+    width: 50px
+    &.active
+        background-color:alpha(black, .25)
+    &.played
+        background-color: alpha(green, .5)
+</style>
