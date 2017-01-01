88777d094c550b58a2e51b14c99adb70f8c57c73
Benjamin Steephenson <benjaminsteephenson@gmail.com>
2017-01-01 17:07:31 -0500

    add distortion, filter, chorus...

--- src/components/Instrument.vue
+++ src/components/Instrument.vue
@@ -7,23 +7,22 @@
         input(type="text", v-model.number='measures')
         label Transpose: 
         input(type="text", v-model.number='transpose')
+        label Velocity %: 
+        input(type="text", v-model.number='volume')
+        label Synth type: 
         select(v-model="synth")
             option sine
             option sawtooth
             option square
             option triangle
-            option fmsine
-            option fmsawtooth
-            option fmsquare
-            option fmtriangle
-            option amsine
-            option amsawtooth
-            option amsquare
-            option amtriangle
-            option fatsine
-            option fatsawtooth
-            option fatsquare
-            option fattriangle
+        label Filter Cutoff Hz: 
+        input(type="text", v-model.number='filterCutoff')
+        label Distortion %: 
+        input(type="text", v-model.number='distortion')
+        label Chorus %: 
+        input(type="text", v-model.number='chorus')
+        label Reverb %: 
+        input(type="text", v-model.number='reverbWet')
         line-c(:beats = "steps", :current-note="currentStep", v-for="(onNotes, pitch) in notes", :pitch = "pitch", :on-notes="onNotes", @toggle-box = "handleToggleBox")
 
         hr
@@ -78,6 +77,11 @@ module.exports = {
             measures: 4
             synth: "sine"
             transpose: 0
+            filterCutoff: 2000
+            reverbWet: 10
+            distortion: 0
+            chorus: 0
+            volume: 100
         }
     computed: {
         currentStep: () ->
@@ -107,6 +111,25 @@ module.exports = {
                     detune: val * 100
                 }
                 })
+        filterCutoff: (val) ->
+            if !isNaN(parseInt(val))
+                this.filter.set({
+                    frequency: val
+                    })
+        reverbWet: (val) ->
+            if !isNaN(parseInt(val))
+                this.reverb.set(wet: val / 100)            
+        distortion: (val) ->
+            if !isNaN(parseInt(val))
+                this.distortionFx.set(wet: val / 100)            
+                this.distortionFx.set(distortion: val / 100)            
+        chorus: (val) ->
+            if !isNaN(parseInt(val))
+                this.chorusFx.set(wet: val / 100) 
+        volume: (val) ->
+            if !isNaN(parseInt(val))
+                this.player.set(volume: val / 100) 
+
     }
     methods: {
         playNextStep: () ->
@@ -114,7 +137,7 @@ module.exports = {
             # console.log this.currentBeat, "currentBeat"
             for pitch, onNotes of this.notes
                 if (this.currentStep in onNotes)
-                    this.player.triggerAttackRelease(pitch)
+                    this.player.triggerAttackRelease(pitch, "8n", "+0", this.volume / 100)
                 # else
                 #     this.player.stop({pitch: pitch})
             # setTimeout(
@@ -128,47 +151,55 @@ module.exports = {
                     this.notes[pitch].splice( index, 1 );
             else
                 this.notes[pitch].push(pos)
-                this.player.triggerAttackRelease(pitch, "8n")
+                this.player.triggerAttackRelease(pitch, "8n", "+0", this.volume / 100)
+
     }
 
     created: () ->
-        # console.log this.notes
-        this.player
-        # saw = new Wad({
-        #     source : 'triangle',
-        #     env     : {   
-        #         attack  : 0.0,  
-        #         decay   : 1.5,  
-        #         sustain : 0.0,  
-        #         hold    : 0.0, #alue, oscillators must be manually stopped by calling their stop() method.
-        #         release : 0.00    
-        #     },
-        #     # delay   : {
-        #     #     delayTime : this.timeBetweenBeats / 1000.0 * 3, 
-        #     #     wet       : .25,
-        #     #     feedback  : .75,
-        #     # },
-        #     filter  : {
-        #         type      : 'lowpass', # What type of filter is applied.
-        #         frequency : 2000,       # The frequency, in hertz, to which the filter is applied.
-        #         q         : 1,         # Q-factor.  No one knows what this does. The default value is 1. Sensible values are from 0 to 10.
-        #     },
-        # })
+
         
-        synth = new Tone.PolySynth(8, Tone.OmniSynth).toMaster();
+        synth = new Tone.PolySynth(8, Tone.OmniSynth)
+
         synth.set({
             "oscillator" : {
                 "type" : this.synth
                 detune: this.transpose * 100
-            },
+            }
+        })
+
+        defaultEnv = {
             "envelope" : {
                 "attack" : 0.01,
                 "decay" : 2.0,
                 "sustain" : 0.0,
                 "release" : 0.9,
             }
-        })
+        }
+
+        synth.set(defaultEnv)
+        this.filter = new Tone.Filter(this.filterCutoff, "lowpass");
+        # synth = new Tone.Noise("pink")
+        # synth.connect(Tone.Master)
+
+        this.synthPlayer = synth
+
+        this.reverb = new Tone.Freeverb()
+        this.reverb.set(wet: this.reverbWet / 100)
+
+        this.distortionFx = new Tone.Distortion()
+        this.distortionFx.set(wet: this.distortion / 100)
+        this.distortionFx.set(distortion: this.distortion / 100)
+
+        this.chorusFx = new Tone.Chorus()
+        this.chorusFx.set(wet: this.chorus / 100)
+
+        synth.connect(this.filter)
+        this.filter.connect(this.distortionFx)
+        this.distortionFx.connect(this.chorusFx)
+        this.chorusFx.connect(this.reverb)
+        this.reverb.connect(Tone.Master)
         this.player = synth
+        this.player.set(volume: this.volume / 100)
         # this.nextTick()
         # console.log(this.players)
 
