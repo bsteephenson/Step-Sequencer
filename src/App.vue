6cf5cca30f87f489b11d3cb4434d47597576c69d
Benjamin Steephenson <benjaminsteephenson@gmail.com>
2018-04-22 11:10:40 -0400

    title

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
     #app
-        h1 Music is fun
+        h1 [Insert better name here]
         button(@click='togglePlay') Play / Stop
         button(@click='instruments++') Add Instrument
         label BPM: 
